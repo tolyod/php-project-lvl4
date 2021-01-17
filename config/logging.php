f8933247c9377d9962e01e730ca30d8f2566e711
anatoliy.poloz <anatoliy.poloz@gmail.com>
2021-01-18 02:37:49 +0300

    rollbar logging config fix

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['rollbar', 'single'],
             'ignore_exceptions' => false,
         ],
 
