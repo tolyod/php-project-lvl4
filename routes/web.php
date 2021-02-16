d3397952b059c6096fbfc91a4fdcc6abadce690d
Anatoliy Poloz <t0ly@ukr.net>
2021-02-16 13:21:42 +0300

    default page pre-setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskStatusController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/', HomeController::class)->only('index');
+Route::resource('task_statuses', TaskStatusController::class);
