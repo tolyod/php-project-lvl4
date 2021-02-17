0207430eaf1d31225c7c7f902e0d69b9d96a0048
Anatoliy Poloz <t0ly@ukr.net>
2021-02-18 02:31:56 +0300

    task db relations

--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -10,4 +10,9 @@ class TaskStatus extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'status_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isCreator(Task $task): bool
+    {
+        return $task->creator->id === $this->id;
+    }
 }
