cfca7a3d425a34ff08fe5b93829c1df53b8a1626
Anatoliy Poloz <t0ly@ukr.net>
2021-02-20 10:33:43 +0300

    fix login button

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -17,6 +17,7 @@ return [
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'login' => 'Вход',
+    'enter' => 'Войти',
     'register' => 'Регистрация',
     'logout' => 'Выход',
     'email' => 'Email',
