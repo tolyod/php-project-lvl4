30bc74e3f85b2ee0762b2dd573b495d7d5416cc5
tolyod <anatoliy.poloz@gmail.com>
2021-03-15 15:46:39 +0300

    active lint class added to navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,13 +34,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
+                        <li class="nav-item{{ Route::currentRouteName() === 'tasks.index' ? ' active' : '' }}">
                             <a class="nav-link" href="{{ route('tasks.index') }}">{{ __('layout.tasks') }}</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item{{ Route::currentRouteName() === 'task_statuses.index' ? ' active' : '' }}">
                             <a class="nav-link" href="{{ route('task_statuses.index') }}">{{ __('layout.task_statuses') }}</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item{{ Route::currentRouteName() === 'labels.index' ? ' active' : '' }}">
                             <a class="nav-link" href="{{ route('labels.index') }}">{{ __('layout.labels') }}</a>
                         </li>
                     </ul>
